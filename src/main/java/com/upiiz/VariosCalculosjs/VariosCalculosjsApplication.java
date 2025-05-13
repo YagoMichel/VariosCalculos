099fe7fea5cf8b9be0574d815bc39418f1ba6d46
Yago <santiagomichelh@gmail.com>
2025-05-13 10:26:01 -0600

    Creando una plantilla de springweb y generando todos la estrucutura del proyecto

---
+++ src/main/java/com/upiiz/VariosCalculosjs/VariosCalculosjsApplication.java
@@ -0,0 +1,13 @@
+package com.upiiz.VariosCalculosjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariosCalculosjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariosCalculosjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upiiz/VariosCalculosjs/VariosCalculosjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.VariosCalculosjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariosCalculosjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
